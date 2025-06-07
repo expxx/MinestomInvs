527d99d04580125fed47bd03a6da583de482ba38
Cam <cam@expx.dev>
2025-06-06 21:34:26 -0600

    feat: central publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,76 @@
+import com.vanniktech.maven.publish.JavadocJar
+import com.vanniktech.maven.publish.KotlinJvm
+import com.vanniktech.maven.publish.SonatypeHost
 import io.gitlab.arturbosch.detekt.Detekt
 import io.gitlab.arturbosch.detekt.DetektCreateBaselineTask
+import java.net.URI
 
 plugins {
     kotlin("jvm") version "2.1.0"
     id("io.gitlab.arturbosch.detekt") version "1.23.7"
     id("com.diffplug.spotless") version "7.0.2"
     id("org.jetbrains.dokka") version "2.0.0"
-    id("maven-publish")
-    id("signing")
+    id("com.vanniktech.maven.publish") version "0.32.0"
     id("java-library")
+    signing
+}
+
+group = "dev.expx"
+version = "1.0.6"
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    configure(KotlinJvm(
+        javadocJar = JavadocJar.Dokka("dokkaGeneratePublicationHtml"),
+        sourcesJar = true
+    ))
+
+    coordinates(project.group.toString(), "minestom-invs", project.version.toString())
+
+    signAllPublications()
+
+    pom {
+        name.set("Minestom Inventories")
+        description.set("A library for creating and managing inventories in Minestom.")
+        inceptionYear.set("2024")
+        url.set("https://github.com/expxx/MinestomInvs")
+        licenses {
+            license {
+                name.set("MIT License")
+                url.set("https://opensource.org/license/mit/")
+            }
+        }
+        developers {
+            developer {
+                id.set("cammyzed")
+                name.set("Cam M.")
+                url.set("https://expx.dev")
+            }
+        }
+        scm {
+            url.set("https://github.com/expxx/MinestomInvs")
+            connection.set("scm:git:github.com/expxx/MinestomInvs.git")
+            developerConnection.set("scm:git:ssh://github.com/expxx/MinestomInvs.git")
+        }
+    }
+}
+
+signing {
+    val signingInMemoryKeyId: String? by project
+    val signingInMemoryKey: String? by project
+    val signingInMemoryPassword: String? by project
+    if (signingInMemoryKeyId != null && signingInMemoryKey != null && signingInMemoryPassword != null) {
+        useInMemoryPgpKeys(signingInMemoryKeyId, signingInMemoryKey, signingInMemoryPassword)
+    } else {
+        useGpgCmd()
+    }
+    sign(publishing.publications)
 }
 
 java {
-    withJavadocJar()
-    withSourcesJar()
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
 }
 
 dokka {
@@ -21,25 +78,11 @@ dokka {
     dokkaSourceSets.main {
         sourceLink {
             localDirectory.set(file("src/main/kotlin"))
-            remoteUrl("https://github.com/expxx/MinestomInvs")
+            remoteUrl.set(URI("https://github.com/expxx/MinestomInvs/tree/main/src/main/kotlin"))
             remoteLineSuffix.set("#L")
         }
         pluginsConfiguration.html {
             footerMessage.set("(c) 2025 The Cavern")
-        }
-    }
-}
-
-group = "dev.expx"
-version = "1.0.6-SNAPSHOT"
-
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            from(components["java"])
-            artifactId = "MinestomInvs"
-            groupId = "dev.expx"
-            version = "1.0.9-SNAPSHOT"
         }
     }
 }
@@ -49,6 +92,7 @@ detekt {
     allRules = false
     baseline = file("config/baseline.xml")
 }
+
 spotless {
     kotlin {
         licenseHeaderFile(file("config/HEADER.txt"))
@@ -56,9 +100,6 @@ spotless {
         targetExclude("src/main/kotlin/dev/expx/minestominvs/util/MiniMessage.kt")
     }
 }
-
-group = "dev.expx"
-version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -68,7 +109,6 @@ dependencies {
     testImplementation(kotlin("test"))
     compileOnly("net.minestom:minestom-snapshots:87f6524aeb")
     compileOnly("net.kyori:adventure-text-minimessage:4.18.0")
-
 
     // Detekt
     detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.23.7")
@@ -86,19 +126,10 @@ tasks {
     withType<DetektCreateBaselineTask>().configureEach {
         jvmTarget = "21"
     }
-    jar {
-        manifest {
-            attributes["Main-Class"] = "host.minestudio.lobby.LobbyMainKt"
-        }
-    }
     task("codeQuality") {
         dependsOn("spotlessCheck", "detekt")
     }
     task("spotless") {
         dependsOn("spotlessApply")
     }
-}
-
-kotlin {
-    jvmToolchain(21)
 }
